ffe933cacda24963c7538a692ff33396d67cd336
Hipetysmaximus <antti.hippelainen@gmail.com>
2020-06-30 23:52:51 +0300

    frontend build

---
+++ build/precache-manifest.8a6e08e51d5c28af6bbb1db28ea396d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b25839f91c89379c525f79fc343650",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcd9e898a368dc692715",
+    "url": "/static/css/main.9359936b.chunk.css"
+  },
+  {
+    "revision": "96fa2a80d0443e24a6d9",
+    "url": "/static/js/2.eb02a478.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eb02a478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcd9e898a368dc692715",
+    "url": "/static/js/main.09ebc72a.chunk.js"
+  },
+  {
+    "revision": "b9db865b90dc1d93d813",
+    "url": "/static/js/runtime-main.0f618fec.js"
+  }
+]);
--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
 
+
 morgan.token('data', function(req, res){
     return (JSON.stringify(req.body))
 })
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 let persons = [
